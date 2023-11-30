38f31603c55d3cf5642d33e24b88941ec81a6311
Bruno Delgado Herrero <delgado.hebru24@triana.salesianos.edu>
2023-11-30 14:29:01 +0100

    Código casi arreglado

--- Ejercicio3/src/Ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/Ejercicio3/Ejercicio3.java
@@ -1,6 +1,6 @@
 package Ejercicio3;
 
-import java.util.Arrays;
+import java.util.Arrays; 
 
 public class Ejercicio3 {
     
@@ -11,14 +11,18 @@ public class Ejercicio3 {
         // Insert an element in 3rd position of the array (index->2, value->5)
         
         int Index_position = 2;
-        int newValue    = 5;
+        int newValue    = 35;
         
         System.out.println("Original Array : "+Arrays.toString(my_array));     
         
-        for(int i=0; i >= Index_position; i++){
-            my_array[i] = my_array[i-1];
-        }
+        
         my_array[Index_position] = newValue;
+        
+        for(int i=Index_position; i >= Index_position; i++){
+            my_array[i] = my_array[i+1];
+        }
+        
+        
         System.out.println("New Array: "+Arrays.toString(my_array));
     }
 }
